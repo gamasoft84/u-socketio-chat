b99896f067eef20138f644e807718599a59860f6
gamasoft84 <richardgama@yahoo.com.mx>
2020-12-03 12:05:30 -0600

    dotenv

--- models/server.js
+++ models/server.js
@@ -2,29 +2,32 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
-const Sockets = require('./sockects');
+const cors = require('cors');
+const Sockets = require('./sockets');
 
 
 class Server {
     constructor() {
         this.app = express();
-        this.port = 3000;
+        this.port = process.env.PORT;
         this.server = http.createServer(this.app);
         this.io = socketio(this.server, { /* Configs */ });
     }
 
     middlewares() {
         this.app.use(express.static(path.resolve(__dirname, '../public')));
+        this.app.use(cors);
     }
 
+
     configSockets() {
-        const sockets = new Sockets(this.io);
+        new Sockets(this.io);
     }
 
     execute() {
         this.middlewares();
         this.configSockets();
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log('corriendo en puerto', this.port);
         });
     }
